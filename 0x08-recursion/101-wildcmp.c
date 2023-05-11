3716165a75a4b3b134801c3b9fca5c4b62547678
Mavisanmuury <anmuury@gmail.com>
2023-05-11 16:21:13 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,11 +6,12 @@
  * @s2: pointer to string params
  * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0' && *2 == '*')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
@@ -23,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
